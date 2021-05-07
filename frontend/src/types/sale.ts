776562f68b45a5271eaec52d2bcf4827a5d8720d
Diego Lima <dimaslsoares@gmail.com>
2021-05-07 13:38:26 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum = {
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: 1495;
+    deals: 684;
 }
